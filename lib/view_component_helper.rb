b9b61f397dbbb3555db353028e80ec34a421e343
yamitake <take.yapr@gmail.com>
2023-07-30 16:04:04 +0900

    first commit

---
+++ lib/view_component_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "view_component_helper/version"
+
+module ViewComponentHelper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/view_component_helper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ViewComponentHelper do
+  it "has a version number" do
+    expect(ViewComponentHelper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
