df2cafea919cf2af27bae5910898bcc925604523
yamitake <take.yapr@gmail.com>
2023-09-22 06:37:27 +0900

    Update gem

--- lib/view_component_helper.rb
+++ lib/view_component_helper.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 require "view_component_helper/view_helper"
+require "view_component_helper/railtie" if defined?(Rails)
 require "view_component_helper/version"
--- lib/view_component_helper/view_helper.rb
+++ lib/view_component_helper/view_helper.rb
@@ -1,15 +1,30 @@
+#
+#
+#
 module ViewComponentHelper
-  def render_view_component(path, *args, **kwargs, &)
-    render component_class_for(path).new(*args, **kwargs, &)
+  def render_view_component(path, *args, **kwargs, &block)
+    render path.classify.constantize.new(*args, **kwargs, &block)
   end
 
   alias render_vc render_view_component
   alias vc render_view_component
 
-  private
+  def self.load_components
+    Dir[Rails.root.join('app/components/**/*.rb')].each do |file|
+      require file
 
-  def component_class_for(name)
-    name.classify.constantize
+      component_path = file.gsub("#{Rails.root.join('app/components')}/", '').gsub('.rb', '')
+      component_class_name = component_path.camelize # don't use classify
+      method_name = component_path.gsub('/', '_')
+
+      p "create #{method_name}-----------------------------------------"
+
+      next unless Object.const_defined?(component_class_name) && component_class_name.constantize < ViewComponent::Base
+
+      define_method(method_name) do |*args, **kwargs, &block|
+        render component_class_name.constantize.new(*args, **kwargs, &block)
+      end
+    end
   end
 end
 
